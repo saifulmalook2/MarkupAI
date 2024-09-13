c34aca78ef845fc33442bb5593b5c1c9460a7acf
Saif Ul Malook <saifulmalook16@gmail.com>
2024-09-13 13:33:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ async def document_exist(evidence_id:str, data: ProjectManagmentExist, headers: 
 
     print("file name", file_name)
 
-    response = check_documents_exist(file_name)
+    response = await check_documents_exist(file_name)
 
     logging.info(f"response{response}")
     # return {"status" : response}
