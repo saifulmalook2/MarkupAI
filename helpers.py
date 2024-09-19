57593fd8aab94f51cb045b22c23943ec21e37d53
Saif Ul Malook <saifulmalook16@gmail.com>
2024-09-19 17:05:42 +0500

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -477,7 +477,8 @@ async def check_file_format(persist_directory: str):
 
 async def create_chain(retriever, model):
     system_prompt = '''You are an expert Software Security Auditor. 
-    Your job is to provide answers relevant to the knowledge base provided.  
+    Your job is to provide detailed, complete answers relevant to the knowledge base provided.  
+    Ensure that your responses are in full sentences and thoroughly address the query based on the documents retrieved.
     Do not provide any information that is not contained in the documents retrieved.
     Always give summarized answers using only the content from the retrieved documents. 
     Greetings are allowed but Do not answer any other general queries, always stay within the scope of the documents. 
